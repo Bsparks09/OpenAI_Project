571bfd02bf7880148c8e94d74af308fa4dc9886b
Alex Ofuya <esigbemi@gmail.com>
2023-02-23 08:47:50 -0500

    Update fetch url

--- client/script.js
+++ client/script.js
@@ -90,7 +90,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {      // https://codex-im0y.onrender.com/
+  const response = await fetch('https://codex-v65w.onrender.com/', {      // https://codex-im0y.onrender.com/
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
